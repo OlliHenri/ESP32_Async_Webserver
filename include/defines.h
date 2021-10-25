3290eebc372cd22af467b3822274a9ec286676c6
Oliver Henrichsen <oliver@velanautica.org>
2021-10-24 22:58:03 -0400

    my commit

---
+++ include/defines.h
@@ -0,0 +1,14 @@
+/*
+    Fileserver defines
+*/
+
+#ifndef _defines_h
+#define _defines_h
+
+const char *ssid = "xxxxxxx";      // set your network SSID here
+const char *password = "xxxxxxxx"; // set your network password here
+const char *hostName = "esp-async";
+const char *http_username = "admin";
+const char *http_password = "admin";
+
+#endif
