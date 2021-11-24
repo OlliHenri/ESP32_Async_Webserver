54c25647a6d5dc4ba511d6ae7d645cc184c37a61
Oliver Henrichsen <oliver@velanautica.org>
2021-11-23 21:03:45 -0400

    still on OTA

--- include/defines.h
+++ include/defines.h
@@ -6,7 +6,7 @@
 #define _defines_h
 
 const char *ssid = "Vela_Nautica_2Ghz"; // set your network SSID here
-const char *password = "oliverabc123%"; // set your network password here
+const char *password = "xxxxxxxx";      // set your network password here
 const char *hostName = "esp-async";
 const char *http_username = "admin";
 const char *http_password = "admin";
